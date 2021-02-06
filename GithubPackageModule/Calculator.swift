a383bc864c248b83af377bea15322048245c2451
Erid <erid.bardhaj@grandcentrix.net>
2021-02-06 14:53:42 +0100

    Create calculator logic

---
+++ GithubPackageModule/Calculator.swift
@@ -0,0 +1,15 @@
+//
+//  Calculator.swift
+//  GithubPackageModule
+//
+//  Created by Erid Bardhaj on 2/6/21.
+//
+
+import Foundation
+
+public class Calculator {
+
+    static public func multiply(lhs: Int, rhs: Int) -> Int {
+        lhs * rhs
+    }
+}
