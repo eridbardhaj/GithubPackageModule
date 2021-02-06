a407bc13b7a64085b4245731c77f0d1076cc2b7f
Erid <erid.bardhaj@grandcentrix.net>
2021-02-06 14:47:16 +0100

    Initial Commit

---
+++ GithubPackageModule/GithubPackageModule.h
@@ -0,0 +1,18 @@
+//
+//  GithubPackageModule.h
+//  GithubPackageModule
+//
+//  Created by Erid Bardhaj on 2/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for GithubPackageModule.
+FOUNDATION_EXPORT double GithubPackageModuleVersionNumber;
+
+//! Project version string for GithubPackageModule.
+FOUNDATION_EXPORT const unsigned char GithubPackageModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GithubPackageModule/PublicHeader.h>
+
+
